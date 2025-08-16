81850ad6621b29907f73fe64302772d2f845c33c
samriddhjaiswal3-sudo <samriddhjaiswal3@gmail.com>
2025-08-16 11:21:03 +0530

    Create app1.py

---
+++ app1.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Simple Data App')
+st.write("This app is a working test version.")
+
+uploaded_file = st.file_uploader("Upload a CSV file", type=['csv'])
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write("Data Preview:")
+    st.dataframe(df.head())
